8f1efd0cbc6b657762efe337dd301f8735b6913f
Ihor Shliakhovyi <blizneci2106@gmail.com>
2024-04-30 10:36:55 +0300

    Homework5

---
+++ src/main/java/Homework_5/Animal.java
@@ -0,0 +1,8 @@
+package Homework_5;
+
+public class Animal {
+    private boolean vegetarian;
+    private String eats;
+    private int noOfLegs;
+    Animal constturcot = new Animal();
+}
---
+++ src/main/java/Homework_5/Cat.java
@@ -0,0 +1,5 @@
+package Homework_5;
+
+public class Cat extends Animal {
+
+}
